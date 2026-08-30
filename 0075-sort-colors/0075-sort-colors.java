ba6064b5c888cc9f5bb8ae5075d4d400b073514b
niharika-deshmukhh <niharikadeshmukh203@gmail.com>
2026-08-30 11:42:04 +0530

    Time: 0 ms (100%), Space: 43.4 MB (76.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,33 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        if(nums.length==1||nums.length==1){
-            return;
-        }
-        HashMap<Integer,Integer>map=new HashMap();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-      int j=0;
-            while(map.containsKey(0)&&map.get(0)!=0){
-                nums[j]=0;
-                map.put(0,map.get(0)-1);
-                j++;
-            }
-             while(map.containsKey(1)&&map.get(1)!=0){
-                nums[j]=1;
-                map.put(1,map.get(1)-1);
-                j++;
-            }
-             while(map.containsKey(2)&&map.get(2)!=0){
-                nums[j]=2;
-                map.put(2,map.get(2)-1);
-                j++;
-            }
-            if(j==nums.length-1){
-              return;
+       
+        int p=0;
+      for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                int temp=nums[i];
+                nums[i]=nums[p];
+                nums[p]=temp;
+                p++;
             }
         }
-
-        
-    
-}
+          
+      for(int j=0;j<nums.length;j++){
+            if(nums[j]==1){
+                int temp=nums[j];
+                nums[j]=nums[p];
+                nums[p]=temp;
+                p++;
+            }
+        }
+        }
+    }
